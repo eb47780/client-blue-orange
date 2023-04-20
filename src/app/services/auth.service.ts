25d4e94a1677186658b10ecc97d1d4e5a5e914a4
endritber <endritpb@gmail.com>
2023-04-20 13:52:27 +0200

    rxjs working fine

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,46 @@
 import { Injectable } from '@angular/core';
-import { StoreService } from './store.service';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, switchMap } from "rxjs/operators";
+import { Observable, of } from 'rxjs';
 import { User } from '../models/user.model';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  user: User;
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService) { }
 
-  constructor(private http: HttpClient) { }
-
-  onLogin(data: any):Observable<any> {
-    return this.http.post(`http://localhost:8001/api/token`, data)
+  login(data: any) {  
+    return this.http.post<any>('http://localhost:8001/api/token', data).pipe(
+      map((token) => {
+        console.log(token)
+        console.log('token: ' + token.access);
+        localStorage.setItem('access', token.access);
+        return token;
+      })
+    )
   }
 
-  getUser(url: string): Observable<any> {
-    return this.http.get(url)
+  logout() {
+    localStorage.removeItem('access');
   }
 
+  register(user: User) {
+    return this.http.post<any>('http://localhost:8001/api/client/v1', user);
+  }
+
+  isAuthenticated(): boolean {
+    const token = localStorage.getItem('access');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+
+  getUserId(): Observable<any>{
+    return of(localStorage.getItem('access')).pipe(
+      switchMap((jwt: any) => of(this.jwtHelper.decodeToken(jwt)).pipe(
+        map((jwt: any) => jwt.user.id)
+      )
+    ));
+  }
 }
