f67914e777712abbdc2020f387e37e00e2a4f2b7
endritber <endritpb@gmail.com>
2023-04-20 13:16:45 +0200

    Create user model, login attempt with headers

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@angular/core';
 import { StoreService } from './store.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  user: User;
+
   constructor(private http: HttpClient) { }
 
-  login(data:any):Observable<any> {
+  onLogin(data: any):Observable<any> {
     return this.http.post(`http://localhost:8001/api/token`, data)
+  }
+
+  getUser(url: string): Observable<any> {
+    return this.http.get(url)
   }
 
 }
